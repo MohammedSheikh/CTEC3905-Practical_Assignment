6d8ba55dc701016de7ca36d9081bd8af0d9ddc90
MohammedSheikh <p14165162@myemail.dmu.ac.uk>
2017-11-19 20:03:09 +0000

    Added a navigation menu; added breakpoints to CSS

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+(function(){
+
+  let menuButton = document.getElementById("mainMenu");
+  let navMenu = document.getElementById("nav-menu");
+
+  menuButton.addEventListener("click", toggleMenu);
+
+  let toggle = false; // hidden at first
+  function toggleMenu(){
+    if (toggle) { // if menu is visible...
+      navMenu.classList.remove("displayMenu"), // ...hide the menu
+      toggle = false
+    }
+    else { // false: it's hidden
+      navMenu.classList.add("displayMenu"), // show the menu
+      toggle = true
+    }
+  }
+
+})();
