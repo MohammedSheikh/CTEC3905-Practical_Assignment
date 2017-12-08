a2a1e0a381f83fb2d925fc2b4886bc9b289d0123
MohammedSheikh <p14165162@myemail.dmu.ac.uk>
2017-12-08 19:08:02 +0000

    Removed 'Top5' page and renamed few elements to include hyphens

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
+//This line of code defines that the JS should be executed strictly, so it will
+//allow only certain code
 "use strict";
 
 (function(){
 
-  let menuButton = document.getElementById("mainMenu");
+  let menuButton = document.getElementById("main-menu");
   let navMenu = document.getElementById("nav-menu");
 
   menuButton.addEventListener("click", toggleMenu);
@@ -10,11 +12,11 @@
   let toggle = false; // hidden at first
   function toggleMenu(){
     if (toggle) { // if menu is visible...
-      navMenu.classList.remove("displayMenu"), // ...hide the menu
+      navMenu.classList.remove("display-menu"), // ...hide the menu
       toggle = false
     }
     else { // false: it's hidden
-      navMenu.classList.add("displayMenu"), // show the menu
+      navMenu.classList.add("display-menu"), // show the menu
       toggle = true
     }
   }
