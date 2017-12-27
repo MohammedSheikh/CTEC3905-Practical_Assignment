910e113b0d222e49a97929f6d31d7e072ab110e7
MohammedSheikh <p14165162@myemail.dmu.ac.uk>
2017-12-27 11:54:16 +0000

    Fixed slideshow arrow button issue

--- js/scripts.js
+++ js/scripts.js
@@ -66,6 +66,34 @@ official code documentation*/
 
 })();
 
+(function(){
+
+  let backArrow = document.getElementById("back-arrow");
+  let forwardArrow = document.getElementById("forward-arrow");
+
+  let slideIndex = 1;
+  showDivs(slideIndex);
+
+  function plusDivs(n) {
+    showDivs(slideIndex += n);
+  }
+
+  function showDivs(n) {
+    let i;
+    let x = document.getElementsByClassName("slides");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
+  }
+
+  backArrow.addEventListener("click", function (){plusDivs(-1)});
+  forwardArrow.addEventListener("click", function (){plusDivs(1)});
+
+})();
+
 //function that implements Google Maps API into the website
 function initMap() {
   //the latitude and logitude of the location
